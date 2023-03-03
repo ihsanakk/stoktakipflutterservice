c98310b1f6b56736445bd5de593a93e6c0896ca6
ihsanakk <aakkoyunlu.ihsan@gmail.com>
2023-03-03 03:26:16 +0300

    init

---
+++ src/main/java/com/tempter/stoktakipservice/StoktakipserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tempter.stoktakipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoktakipserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoktakipserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempter/stoktakipservice/StoktakipserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempter.stoktakipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoktakipserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
